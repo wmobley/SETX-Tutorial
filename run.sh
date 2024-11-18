f1ae048e403bb4159b20f2f7503f5aa06eb708e0
Will Mobley <feulmob+github@gmail.com>
2024-11-18 14:52:44 -0600

    Add appropriate repo url

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ set -xe
 # COOKBOOK_NAME: Name of the cookbook
 # COOKBOOK_CONDA_ENV: Name of the conda environment
 # IS_GPU_JOB: Boolean value to indicate if the job is a GPU job. If true, it will load the CUDA module
-export GIT_REPO_URL="https://github.com/In-For-Disaster-Analytics/Cookbook-Jupyter-Template.git"
+export GIT_REPO_URL="https://github.com/wmobley/SETX-Tutorial.git"
 export COOKBOOK_NAME="cookbook-template-jupyter"
-export COOKBOOK_CONDA_ENV="example"
+export COOKBOOK_CONDA_ENV="Setx Tutorial"
 IS_GPU_JOB=false
 
 
